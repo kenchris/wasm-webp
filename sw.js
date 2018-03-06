9c40f614f017d874eb9a0f013fbcc9f0656db683
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2018-03-06 18:08:21 +0100

    A bit more work

--- sw.js
+++ sw.js
@@ -2,29 +2,29 @@ var Module = {
   ENVIRONMENT: "WORKER"
 };
 
-
 async function fetchAndInstantiate() {
   const response = await fetch('webp_wasm.wasm');
   Module["wasmBinary"] = await response.arrayBuffer();
   importScripts('webp_wasm.js');
 
-  const getInfo = Module.cwrap('getInfo', 'number', ['number', 'number']);
+  const _getInfo = Module.cwrap('getInfo', 'number', ['number', 'number']);
+  const getInfo = (buffer, size) => {
+    const ptr = _getInfo(buffer, size);
+    const success = !!Module.getValue(ptr, "i32");
 
-  return {
-    decodeWebPBufferToCanvas: Module.cwrap('version', 'number', []),
-    createUint8Buffer: Module.cwrap('createUint8Buffer', 'number', ['number']),
-    getInfo: (buffer, size) => {
-      const ptr = getInfo(buffer, size);
-      const success = !!Module.getValue(ptr, "i32");
+    if (!success) return { width: null, height: null };
 
-      if (!success) return { width: null, height: null };
+    const width = Module.getValue(ptr + 4, "i32");
+    const height = Module.getValue(ptr + 8, "i32");
 
-      const width = Module.getValue(ptr + 4, "i32");
-      const height = Module.getValue(ptr + 8, "i32");
-
-      return { width, height };
-    }
+    return { width, height };
   }
+  const version = Module.cwrap('version', 'number', []);
+  const createUint8Buffer = Module.cwrap('createUint8Buffer', 'number', ['number']);
+  const decode = Module.cwrap('decode', 'number', ['number', 'number']);
+  const destroy = Module.cwrap('destroy', 'void', ['number']);
+
+  return { version, getInfo, createUint8Buffer, decode, destroy };
 }
 
 const moduleReady = (async () => {
@@ -43,23 +43,24 @@ self.addEventListener('fetch', async event => {
 
     try {
       await moduleReady;
-      const version = Module.exports.decodeWebPBufferToCanvas();
+      const version = Module.exports.version();
+      console.log("webp version", version)
 
       const ptr = Module.exports.createUint8Buffer(buffer.byteLength);
       console.log(ptr, buffer.buffer, buffer.byteLength, buffer.length);
       Module.HEAP8.set(buffer.buffer, ptr);
 
-      console.log(Module.exports.getInfo(ptr, buffer.byteLength));
+      const info = Module.exports.getInfo(ptr, buffer.byteLength);
+      console.log(info);
 
-      //let blob = null;
-      //if (Module.canvas.convertToBlob) {
-      //  blob = await Module.canvas.convertToBlob({ type: "image/png", quality: 1});
-      //} else {
-      //  blob = await Module.canvas.toBlob("image/png", 1);
-      //}
+      const resultPtr = Module.exports.decode(ptr, buffer.byteLength);
+      const resultView = new Uint8Array(Module.HEAP8.buffer, resultPtr, info.width * info.height * 4);
+      const result = new Uint8Array(resultView);
+      Module.exports.destroy(resultPtr);
 
-      //return new Response(blob, { "status": 200 });
-      console.log(version);
+      console.log(result);
+
+      return new Response(result, { "status": 200 });
     } catch(err) {
       console.error(err);
     }
--- webp.c
+++ webp.c
@@ -20,10 +20,30 @@ void destroyUint8Buffer(uint8_t* p) {
 }
 
 EMSCRIPTEN_KEEPALIVE
-int* getInfo(const uint8_t* data, size_t data_size) {
+int* getInfo(const uint8_t* data, size_t size) {
   int* results = (int*) malloc(3 * sizeof(int));
 
-  results[0] = WebPGetInfo(data, data_size, results + 1, results + 2);
+  int width;
+  int height;
+
+  // (const uint8_t* data, size_t size, int* w, int* h) -> int;
+  results[0] = WebPGetInfo(data, size, &width, &height);
+  results[1] = width;
+  results[2] = height;
 
   return results;
+}
+
+EMSCRIPTEN_KEEPALIVE
+uint8_t* decode(const uint8_t* data, size_t size) {
+  int width;
+  int height;
+
+  uint8_t* buffer = WebPDecodeRGBA(data, size, &width, &height);
+  return buffer;
+}
+
+EMSCRIPTEN_KEEPALIVE
+void destroy(const uint8_t* data) {
+  WebPFree((void *)data);
 }
