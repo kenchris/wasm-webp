7678f78db0c0e61858add856d4e516aa8cb31687
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2018-03-06 19:58:17 +0100

    Now working WebP decoder in SW!

--- sw.js
+++ sw.js
@@ -11,9 +11,9 @@ function importWebP() {
 
       class WebPDecoder {
         constructor(buffer) {
-          this.size = buffer.length * buffer.BYTES_PER_ELEMENT;
+          this.size = buffer.byteLength;
           this.ptr = Module._malloc(this.size);
-          Module.HEAPU8.set(buffer, this.ptr);
+          Module.HEAPU8.set(new Uint8Array(buffer), this.ptr);
         }
 
         version() {
@@ -39,8 +39,8 @@ function importWebP() {
           let { width, height } = this.info();
 
           const resultPtr = decode(this.ptr, this.size);
-          const resultView = new Uint8Array(Module.HEAP8.buffer, resultPtr, width * height * 4);
-          const result = new Uint8Array(resultView);
+          const resultView = new Uint8Array(Module.HEAPU8.buffer, resultPtr, width * height * 4);
+          const result = new Uint8ClampedArray(resultView);
           Module._free(resultPtr);
 
           return result;
@@ -52,6 +52,16 @@ function importWebP() {
   });
 }
 
+let resolver;
+const canvasReady = new Promise(resolve => {
+  resolver = resolve;
+});
+
+self.addEventListener('message', event => {
+  Module.canvas = event.data.canvas;
+  resolver();
+});
+
 self.addEventListener('fetch', async event => {
   if (event.request.method != 'GET') return;
 
@@ -60,19 +70,37 @@ self.addEventListener('fetch', async event => {
   event.respondWith(async function() {
     const response = await fetch(event.request);
 
-    const buffer = new Uint8Array(await response.arrayBuffer());
+    const buffer = await response.arrayBuffer();
+    console.log(buffer);
+
+    await canvasReady;
 
     try {
       const { WebPDecoder } = await importWebP();
 
-      const decoder = new WebPDecoder(buffer.buffer, buffer.byteLength);
+      const decoder = new WebPDecoder(buffer);
 
       console.log("WebP version", decoder.version());
-      console.log("Info", decoder.info());
+      let { width, height } = decoder.info();
+      console.log("Info", width, height);
       const result = decoder.decode();
       console.log("Decoded", result);
 
-      return new Response(result, { "status": 200 });
+      const img = new ImageData(result, width, height);
+
+      Module.canvas.width = img.width;
+      Module.canvas.height = img.height;
+      const ctx = Module.canvas.getContext('2d');
+      ctx.putImageData(img, 0, 0);
+
+      let blob = null;
+      if (Module.canvas.convertToBlob) {
+        blob = await Module.canvas.convertToBlob({ type: "image/png", quality: 1});
+      } else {
+        blob = await Module.canvas.toBlob("image/png", 1);
+      }
+
+      return new Response(blob, { "status": 200 });
     } catch(err) {
       console.error(err);
     }
